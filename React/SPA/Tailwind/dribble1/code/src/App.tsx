5cbd2767a782c813b0f75828c50979bddeefe127
stephen-dio <stephendiodev@gmail.com>
2022-04-28 13:56:19 -0400

    init tailwind before craco

---
+++ React/SPA/Tailwind/dribble1/code/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import samplePage from './components/samplePage';
+
+function App() {
+  return (
+    <>
+      <samplePage/>
+    </>
+  );
+}
+
+export default App;
